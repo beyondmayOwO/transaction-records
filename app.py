2bfa28d45cccd19f21204cf04b183179cfffca98
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-01-11 21:17:18 +0630

    Initial set up with sample data

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 # Import libraries
+from flask import Flask, request, url_for, redirect, render_template
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
-
+@app.route('/')
+def run():
+    return "hello"
 # Create operation
 
 # Update operation
@@ -13,4 +22,5 @@
 # Delete operation
 
 # Run the Flask app
-    
+if __name__ == "__main__":
+    app.run(debug=True)
